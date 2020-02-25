c62c4a3548a75d5ca2041d4e9f0af637237c21ed
Lzzz <583559075@qq.com>
2020-02-25 10:01:01 +0800

    hello

--- LeetCode_Go/20.有效的括号/code.go
+++ LeetCode_Go/20.有效的括号/code.go
@@ -5,9 +5,34 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println(isValid("{[()]}"))
 }
 
 func isValid(s string) bool {
+	if s == "" {
+		return true
+	} else {
+		if len(s)%2 != 0 {
+			return false
+		}
+	}
+	stack := make([]int32, len(s))
+	stackLength := 0
+	for _, v := range s {
+		if v == '(' || v == '[' || v == '{' {
+			stack[stackLength] = v
+			stackLength++
+		} else {
+			if stackLength == 0 {
+				return false
+			}
+			if (v == ')' && stack[stackLength-1] == '(') || (v == ']' && stack[stackLength-1] == '[') || (v == '}' && stack[stackLength-1] == '{') {
+				stackLength--
+			} else {
+				return false
+			}
+		}
 
+	}
+	return stackLength == 0
 }
