969974cd7e46c4754df7c93cceed57750e233ec3
Lzzz <583559075@qq.com>
2020-02-26 20:32:51 +0800

    2.26 简化算法

--- LeetCode_Go/20.有效的括号/code.go
+++ LeetCode_Go/20.有效的括号/code.go
@@ -5,16 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println(isValid("{[()]}"))
+	fmt.Println(isValid2("{[()]}"))
 }
 
 func isValid(s string) bool {
 	if s == "" {
 		return true
-	} else {
-		if len(s)%2 != 0 {
-			return false
-		}
+	}
+	if len(s)%2 != 0 {
+		return false
 	}
 	stack := make([]int32, len(s))
 	stackLength := 0
@@ -33,6 +32,33 @@ func isValid(s string) bool {
 			}
 		}
 
+	}
+	return stackLength == 0
+}
+
+func isValid2(s string) bool {
+	if s == "" {
+		return true
+	}
+	if len(s)%2 != 0 {
+		return false
+	}
+	var stack []byte
+	stackLength := len(stack)
+	mymap := map[byte]byte{')': '(', ']': '[', '}': '{'}
+	for i := 0; i < len(s); i++ {
+		if len(stack) > 0 {
+			if _, dp := mymap[byte(s[i])]; dp {
+				if stack[stackLength-1] == mymap[byte(s[i])] {
+					stack = stack[:stackLength-1]
+					stackLength--
+					continue
+				}
+			}
+		}
+		stackLength++
+		stack = append(stack, byte(s[i]))
+		fmt.Println("length: ", stackLength)
 	}
 	return stackLength == 0
 }
